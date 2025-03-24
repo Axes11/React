02064a98595a6ef7c1d1179a1daba05c60785552
Ivan <123070177+Axes11@users.noreply.github.com>
2025-03-24 22:25:46 +0100

    Learn Next about routing on page

---
+++ learn-next/src/app/products/[productid]/reviews/[reviewid]/page.tsx
@@ -0,0 +1,13 @@
+import NotFound, { notFound } from "next/navigation";
+
+export default function ReviewDetail({params}: {
+      params: {
+            productid: string;
+            reviewid: string;
+      }
+}){
+      if(parseInt(params.reviewid) > 1000){
+            notFound();
+      }
+      return <h1>Review - {params.reviewid} for product {params.productid}</h1>
+}
